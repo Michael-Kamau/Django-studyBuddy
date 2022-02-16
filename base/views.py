72dadbf476fc3ea29edeae5fb9bc7a89abc3c016
Michael Kamau <kamau.karitu@gmail.com>
2022-02-16 13:02:01 +0300

    Pass data to template using a context variable and display it

--- base/views.py
+++ base/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+rooms = [
+    {'id':1, 'name':'Welcome to the future of programming'},
+    {'id':2, 'name':'Another of the best tutorials'},
+    {'id':3, 'name':'Never give up Mot'}
+]
+
 def home(request):
-    return render(request,'home.html')
+    context = {'rooms':rooms}
+    return render(request,'home.html',context)
 
 def room(request):
     return render(request,'room.html')

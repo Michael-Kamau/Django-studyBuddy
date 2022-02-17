e204a3ac3872c4250f0e46b565a8b6a054d96450
Michael Kamau <kamau.karitu@gmail.com>
2022-02-17 14:05:21 +0300

    Authentication restrictioins:  - Limit access to create and delete rooms to only authenticated users  - Limit login page access to only unauthenticated users  - Make delete room and update room button  available to only room owners  - Restrics delete room and update room functionalities to the room owner.

--- base/views.py
+++ base/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
@@ -15,6 +17,9 @@ from .models import Room, Topic
 #     {'id':3, 'name':'Never give up Mot'}
 # ]
 def loginPage(request):
+
+    if request.user.is_authenticated:
+        return redirect('home')
 
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -41,7 +46,7 @@ def logoutUser(request):
     logout(request)
     return redirect('home')
 
-    
+
 def home(request):
 
     q = request.GET.get('q') if request.GET.get('q') != None else ''
@@ -66,6 +71,7 @@ def room(request, pk):
     context = {'room':room}
     return render(request,'base/room.html', context)
 
+@login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
     if request.method == 'POST':
@@ -76,9 +82,14 @@ def createRoom(request):
     context = {'form':form}
     return render(request,'base/room_form.html', context)
 
+@login_required(login_url='login')
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance = room)
+
+    if request.user != room.host:
+        return HttpResponse ('You are not allowed here')
+
     if request.method == 'POST':
         form = RoomForm(request.POST, instance = room)
         if form.is_valid():
@@ -89,8 +100,12 @@ def updateRoom(request, pk):
 
     return render(request,'base/room_form.html', context)
 
+@login_required(login_url='login')
 def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
+
+    if request.user != room.host:
+        return HttpResponse ('You are not allowed here')
     if request.method == 'POST':
         room.delete()
         return redirect('home')

f244c950f596df456d3b7b05bd649722feaefe7f
Michael Kamau <kamau.karitu@gmail.com>
2022-02-17 16:26:29 +0300

    Show activity feed in homepage

--- base/views.py
+++ base/views.py
@@ -85,8 +85,10 @@ def home(request):
 
     topics = Topic.objects.all()
     room_count = rooms.count()
+    room_messages = Message.objects.all()
 
-    context = {'rooms':rooms, 'topics':topics, 'room_count':room_count}
+
+    context = {'rooms':rooms, 'topics':topics, 'room_count':room_count, 'room_messages':room_messages}
     return render(request,'base/home.html',context)
 
 def room(request, pk):
@@ -147,3 +149,14 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request,'base/delete.html',{'obj':room})
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse ('You are not allowed here')
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request,'base/delete.html',{'obj':message})

6d733bc8daea66b23127576f8297bec28aed449e
Michael Kamau <kamau.karitu@gmail.com>
2022-03-22 15:48:09 +0300

    Add activities component for mobile view

--- base/urls.py
+++ base/urls.py
@@ -17,7 +17,8 @@ urlpatterns = [
 
 
     path("update-user/<str:pk>", views.updateUser, name="update-user"),
-
+ 
     path("topics/", views.topicsPage, name="topics"),
+    path("activity/", views.activityPage, name="activity"),
 
 ]

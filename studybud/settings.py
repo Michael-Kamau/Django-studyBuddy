f701039934cfb59535390e9f9cc4c07b160807f4
Michael Kamau <kamau.karitu@gmail.com>
2022-03-23 13:18:11 +0300

    Install django-cors-headers and allow all origins

--- studybud/settings.py
+++ studybud/settings.py
@@ -39,8 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'base.apps.BaseConfig',
-    
-    'rest_framework'
+
+    'rest_framework',
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,5 @@ STATICFILES_DIRS =[
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
